b9ecfc9f1ff89a4b56a35a2ea781d9e503204e46
smq2021 <mx9223@gmail.com>
2024-08-04 21:36:20 -0400

    Working code

--- app/src/main/java/com/example/myalarm/MainActivity.kt
+++ app/src/main/java/com/example/myalarm/MainActivity.kt
@@ -1,48 +1,72 @@
 package com.example.myalarm
 
+import android.media.Ringtone
+import android.media.RingtoneManager
 import android.os.Bundle
+
+import android.widget.TextClock
+import android.widget.TimePicker
 import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.myalarm.ui.theme.MyAlarmTheme
+import com.example.myalarm.R
+
+import java.util.Timer
+import java.util.TimerTask
 
 class MainActivity : ComponentActivity() {
+
+    private lateinit var alarmTime: TimePicker
+    private lateinit var currentTime: TextClock
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        enableEdgeToEdge()
-        /*setContent {
-            MyAlarmTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+
+        alarmTime = findViewById(R.id.time_picker)
+        currentTime = findViewById(R.id.textClock2)
+        val r: Ringtone = RingtoneManager.getRingtone(applicationContext, RingtoneManager.getDefaultUri(RingtoneManager.TYPE_RINGTONE))
+
+        val t = Timer()
+        t.schedule(object : TimerTask() {
+            override fun run() {
+                runOnUiThread {
+                    currentTime.format12Hour = "h:mm a"
+                    if (currentTime.text.toString() == alarmTime() ) {
+                        r.play()
+                    } else {
+                        r.stop()
+                    }
                 }
             }
-        }*/
+        }, 0, 1000)
+    }
+
+    private fun alarmTime(): String {
+        var alarmHours = alarmTime.hour
+        val alarmMinutes = alarmTime.minute
+        val stringAlarmMinutes: String
+        val stringAlarmHours: String
+
+        stringAlarmHours = if (alarmHours < 10) {
+            "0$alarmHours"
+        } else {
+            alarmHours.toString()
+        }
+
+        stringAlarmMinutes = if (alarmMinutes < 10) {
+            "0$alarmMinutes"
+        } else {
+            alarmMinutes.toString()
+        }
+
+        val stringAlarmTime: String = "$stringAlarmHours:$stringAlarmMinutes"
+
+//        val stringAlarmTime: String = if (alarmHours > 12) {
+//            alarmHours -= 12
+//            "$stringAlarmHours:$stringAlarmMinutes PM"
+//        } else {
+//            "$stringAlarmHours:$stringAlarmMinutes AM"
+//        }
+
+        return stringAlarmTime
     }
 }
-
-/*@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    MyAlarmTheme {
-        Greeting("Android")
-    }
-}*/
